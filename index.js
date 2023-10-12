adc418778b55f9f89378709fc32a1b3a1852797a
unknown <oyindamolacomfort@gmail.com>
2023-10-11 21:09:55 -0700

    work on javascrprit

---
+++ index.js
@@ -0,0 +1,247 @@
+let agegroup = 2 ;
+let lastname = "oyin";
+
+
+console.log(agegroup);
+console.log(lastname);
+
+console.log(agegroup + "" +lastname);
+
+
+let age = 45; 
+
+age = 3;
+console.log(age);
+
+
+function subtract(num35, num3) {
+     console.log(num35 - num3);
+
+}
+
+subtract(35, 3);
+
+
+function division(num4, num5){
+    console.log(num4/num5);
+
+}
+division(4, 5);
+
+
+
+
+
+
+function concatenate(str1, str2)  {
+    console.log(str1 + str2);
+}concatenate("car", "battery");
+
+const personA ={
+    firstname: 'oyindamola',
+    like:  'pizza'
+};
+
+
+const personB ={
+    firstaname: 'declan',
+    likes: 'plantain'
+};
+
+
+
+
+
+
+console.log("Before")
+console.log(personA.firstname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("calculate averge");
+let testscorce1 =5;
+let testscorce2 =9;
+let testscorce3 =7;
+
+
+
+function calculateaverage() {
+
+    console.log(testscorce1);
+    console.log(testscorce2);
+    console.log(testscorce3);
+    const total = (testscorce1 + testscorce2 + testscorce3);
+    console.log(total);
+
+};
+calculateaverage();
+
+
+
+function addition (testscorce1, testscorce2, testscorce3){
+    //cosole log (testscrore1 + testscorce2 + testscore3);
+    return testscorce1+testscorce2+testscorce3;
+};
+
+
+
+age = 13;
+let minimumdriverage = 18;
+
+let oldenoughtodrive=age >= minimumdriverage;
+
+console.log("are you old enough to drive?" +oldenoughtodrive);
+
+
+
+
+
+
+
+
+  //logical operators AND(&&),OR(||), NOT(!)
+    //AND results in true when all the condition result true esle it returns false 
+    // OR results  in true when ate least one of the condtion returns true, esle it returns to false 
+
+    mark = 70;
+    // if else if statement
+    if(mark >= 90) {
+        // code to execute
+        console.log("A+");
+    } else if (mark >= 80 && mark < 90) {
+        // code to execute
+        console.log("A");
+    } else if (mark >= 70 && mark < 80) {
+        // code to exceute
+        console.log("B");
+    } else if (mark >=60 && mark < 70) {
+       console.log("c");
+    } else if (mark >=50 && mark < 60) {
+       console.log("D");
+    } else if (mark >40 && mark <50 )  {
+        console.log("E");
+    } else if (mark >30 && mark <40 )  {
+
+    }else{
+        //
+        console.log("A+, B, C, D, E,");
+    }
+
+
+    
+    // an array of 4 names 
+    let namesArray = ["oyindamola","Nomakhosi","Elisah"];
+
+    let personArray = ["shakira",23, 6.65, { canDance: true }];
+
+    console.log(namesArray[3]);
+
+
+    console.log("The number of elements inside the Names' Array:"+namesArray.lenght)
+    
+    for(let i=0; i<namesArray.length; i++) {
+        console.log(namesArray[i]); // Shakirah, Nomakhosi, Elisha, Oyindamola
+    }
+   
+    //Week 2 – JavaScript (Assignment) 
+ 
+    
+
+
+
+
+
+    // code of pratice 
+    //chanllge 1
+
+
+
+    function checkEvenOrOdd(num) {
+        if(num % 2 === 0) {
+            console.log(num+ " is an even number");
+        } else {
+            console.log(num+ " is an odd number");
+        }
+    }
+
+    
+    let timer= 10;
+    for (let a = 10; a>= 0; a--)  {
+        if (a=== 0) {
+            console.log ("blast off");
+        } else {
+            console.log(a);
+        }
+    }
+     //or you can use this method 
+    function countdown(){
+
+    }
+    for (let a = 10; a >= 1; a--) {
+        console.log(a);
+    }
+    console.log("Blast Off!");
+  // checkevenorodd(2) ;
+  // checkEvenorodd(3)  ;
+  countdown();
+
+
+  const shoppingcart = [25, 25, 35,40,20];
+    let sum = 0;
+
+function calculatesum(){
+    for (let i=0; i<shoppingcart.lengh; i++){
+        sum = sum + shoppingcart(i);
+    }
+    console.log(sum);
+
+}
+
+calculatesum();
+
+
+// task 2
+// an array of 4 names 
+let firstaname = ["oyindamola", "Comfort", "Daniel"];
+let help = "Good Morning "
+
+
+
+
+
+// sorting function 
+
+
+
+namesArray.push("anuoluwapo"); 
+namesArray.unshift("sonia");
+
+
+
+//LOOPS AGAIN
+
+
+
+// having access to our html element
+Selection.textcontent ="This is all about me";
+
+let paragraph = "<p>This is a paragraph</p>";
+
+
+
+
+
+
